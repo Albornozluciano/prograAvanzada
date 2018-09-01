951b772bb5931f4e3a8acba9fc10ea7a9b29dd0b
Luciano <lalbornoz@sysone.com>
2018-09-01 20:11:23 -0300

    fixed

--- src/WordWrapper.java
+++ src/WordWrapper.java
@@ -7,17 +7,17 @@ public class WordWrapper {
 
         int espacio = linea.substring(0, i).lastIndexOf(' ');
         if(espacio != -1){
-            return calcular(linea, i, espacio);
+            return calcular(linea,espacio,i);
         }
 
         if (linea.charAt(i)  == ' '){
-            return calcular(linea, i, espacio);
+            return calcular(linea,i,i);
         }
 
         return linea.substring(0,i) + "\n" + wrap(linea.substring(i), i);
     }
 
-    public static String calcular(String linea, int i, int espacio) {
+    public static String calcular(String linea, int espacio, int i) {
         return linea.substring(0, espacio) + "\n" + wrap(linea.substring(espacio + 1), i);
     }
 
